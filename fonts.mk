a314ae4af9784c883c7a160d5f35636fc3495233
Praditia Nur <pnplusplus@gmail.com>
2024-07-05 14:53:58 +0000

    fonts-extra: Import {FiraSans,Fredoka,GoogleSans,Inter,OPlusSans} fonts
    Co-authored-by: kdrag0n <danny@kdrag0n.dev>
    Co-authored-by: Sourajit Karmakar <sourajit@live.com>
    Change-Id: I697560abdc897cb84c8cedb88b3bbadfd9b76dc0

---
+++ fonts.mk
@@ -0,0 +1,30 @@
+# Fira Sans
+PRODUCT_COPY_FILES += \
+    external/fonts-extra/FiraSans-Bold.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/FiraSans-Bold.ttf \
+    external/fonts-extra/FiraSans-BoldItalic.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/FiraSans-BoldItalic.ttf \
+    external/fonts-extra/FiraSans-Italic.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/FiraSans-Italic.ttf \
+    external/fonts-extra/FiraSans-Light.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/FiraSans-Light.ttf \
+    external/fonts-extra/FiraSans-LightItalic.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/FiraSans-LightItalic.ttf \
+    external/fonts-extra/FiraSans-Medium.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/FiraSans-Medium.ttf \
+    external/fonts-extra/FiraSans-MediumItalic.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/FiraSans-MediumItalic.ttf \
+    external/fonts-extra/FiraSans-Regular.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/FiraSans-Regular.ttf
+
+# Fredoka
+PRODUCT_COPY_FILES += \
+    external/fonts-extra/Fredoka-VariableFont_wdth_wght.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/Fredoka-VariableFont_wdth_wght.ttf
+
+# Google Sans
+# NOTES: Maybe this is one of not open source or free to use font in this list, but we are using Android from Google, so i guess it's fine
+PRODUCT_COPY_FILES += \
+    external/fonts-extra/GoogleSans-Italic-VF.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/GoogleSans-Italic-VF.ttf \
+    external/fonts-extra/GoogleSans-Regular-VF.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/GoogleSans-Regular-VF.ttf
+
+# Inter
+PRODUCT_COPY_FILES += \
+    external/fonts-extra/Inter-Regular.otf:$(TARGET_COPY_OUT_PRODUCT)/fonts/Inter-Regular.otf \
+    external/fonts-extra/Inter-Regular-VF.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/Inter-Regular-VF.ttf
+
+# OPlus Sans
+# FONT USAGE DISCLAIMER: https://twitter.com/OPPOIndonesia/status/1306520096052072448
+PRODUCT_COPY_FILES += \
+    external/fonts-extra/OPlusSans-Regular.ttf:$(TARGET_COPY_OUT_PRODUCT)/fonts/OPlusSans-Regular.ttf
